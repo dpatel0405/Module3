42c38210b1500cd7d26127294c8996cd94532812
hello_World <dpatel04@terpmail.umd.edu>
2026-09-25 13:55:57 -0400

    Created about html page and added it to app.py

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Deven Patel! Testing output'
+    return 'Hello World from Deven Patel! I am adding my first code change'
 
 @app.route('/hello')
 def hello():
     return render_template('helloWorld.html')
+
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
